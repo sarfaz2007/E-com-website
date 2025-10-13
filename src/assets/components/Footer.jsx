bb6c83065a95cceb9da4898aefec7e4e1f9f4655
sarfaz <sarfazsarfaz646@gmail.com>
2025-10-13 13:57:23 +0530

    day-3

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -3,51 +3,53 @@ import React from 'react'
 function Footer() {
     return (
         <div className='w-full mx-auto bg-black'>
-            <div className='flex justify-between mt-6 py-16 px-40'>
+            <div className='flex flex-wrap justify-between mt-6 py-16 px-40'>
                 <div>
                     <h6 className='font-medium text-white text-2xl pb-5'>Exclusive</h6>
                     <ul>
                         <li className='py-2 text-xl text-white'>Subscribe</li>
                         <li className='py-2 text-xl text-white'>Marketing</li>
-                        <input className='p-3 flex w-full rounded-md text-black  bg-white' type="email" placeholder='Enter email' />
-
-                        
+                        <input className='p-3 flex w-full rounded-md text-white  bg-black border-b-1' type="email" placeholder='Enter email' />
                     </ul>
                 </div>
                 <div>
                     <h6 className='font-medium text-white text-2xl pb-5'>Support</h6>
                     <ul>
-                        <li className='py-2 text-xl text-white'>Pricing</li>
-                        <li className='py-2 text-xl text-white'>Documentation</li>
-                        <li className='py-2 text-xl text-white'>Guides</li>
-                        <li className='py-2 text-xl text-white'>API Status</li>
+                        <li className='py-2 text-xl text-white'>111 Bijoy sarani,Dhaka, <br />DH 1515,Bangladhesh</li>
+                        <li className='py-2 text-xl text-white'>Exclusive@gmail.com</li>
+                        <li className='py-2 text-xl text-white'>+88015-88888-9999</li>
                     </ul>
                 </div>
                 <div>
                     <h6 className='font-medium text-white text-2xl pb-5'>Account</h6>
                     <ul>
-                        <li className='py-2 text-xl text-white'>About</li>
-                        <li className='py-2 text-xl text-white'>Blogs</li>
-                        <li className='py-2 text-xl text-white'>Jobs</li>
-                        <li className='py-2 text-xl text-white'>Press</li>
-                        <li className='py-2 text-xl text-white'>career</li>
+                        <li className='py-2 text-xl text-white'>My Account</li>
+                        <li className='py-2 text-xl text-white'>Login/Register</li>
+                        <li className='py-2 text-xl text-white'>cart</li>
+                        <li className='py-2 text-xl text-white'>Wishlist</li>
+                        <li className='py-2 text-xl text-white'>Shop</li>
                     </ul>
                 </div>
                 <div>
                     <h6 className='font-medium text-white text-2xl pb-5'>Quick Link</h6>
                     <ul>
-                        <li className='py-2 text-xl text-white'>Claim</li>
-                        <li className='py-2 text-xl text-white'>Policy</li>
-                        <li className='py-2 text-xl text-white'>Terms</li>
+                        <li className='py-2 text-xl text-white'>Privacy/policy</li>
+                        <li className='py-2 text-xl text-white'>Terms of Use</li>
+                        <li className='py-2 text-xl text-white'>FAQ</li>
+                        <li className='py-2 text-xl text-white'>Contact</li>
                     </ul>
                 </div>
                 <div>
                     <h6 className='font-medium text-white text-2xl pb-5'>Download App</h6>
-                    <ul>
-                        <li className='py-2 text-xl text-white'>Claim</li>
-                        <li className='py-2 text-xl text-white'>Policy</li>
-                        <li className='py-2 text-xl text-white'>Terms</li>
-                    </ul>
+                    <div className='flex gap-4'>
+                        <img src="src/assets/Qrcode 1.svg" alt="" className='' />
+                        <div className=''>
+                            <img src="src/assets/google playstore.png" alt="" className='mb-2.5' />
+                            <img src="src/assets/download-appstore.png" alt="" />
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
