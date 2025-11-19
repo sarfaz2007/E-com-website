2af9f749d043eb2987b20f5c8e8aa5543afc67a2
sarfaz <sarfazsarfaz646@gmail.com>
2025-11-19 14:01:45 +0530

    19/11/25

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 function About() {
   return (
-    <div>fertergdrgdrgrtgretg</div>
+    <>
+      <div className='my-10 ml-14'>
+        <div className='flex w-[80%]'>
+          <p>Home <span>/</span></p>
+          <p>About</p>
+        </div>
+        <div className='w-full flex items-center justify-between'>
+          <div className='w-1/2 '>
+            <h1 className='text-5xl font-semibold py-8'>Our Story</h1>
+            <p className='pb-5 text-xl'>Launced in 2015, Exclusive is South Asia’s premier online shopping <br />
+             makterplace with an active presense in Bangladesh. Supported <br /> 
+             by wide range of tailored marketing, data and service solutions, <br />
+              Exclusive has 10,500 sallers and 300 brands and serves 3 <br />
+               millioons customers across the region. </p>
+            <p className='text-xl'>Exclusive has more than 1 Million products to offer, growing at a <br /> 
+                very fast. Exclusive offers a diverse assotment in categories <br />
+                 ranging  from consumer.</p>
+          </div>
+          <div>
+            <img src="src/assets/portrait-two-african-females-holding-shopping-bags-while-reacting-something-their-smartphone 1.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+
+    </>
   )
 }
 
--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const Account = () => {
   return (
@@ -6,24 +8,81 @@ const Account = () => {
       <div className='w-[80%] mx-auto my-14'>
         <div className='flex justify-between'>
           <div className='flex gap-2'>
-            <p>Home <span>/</span></p>
-            <p>Account</p>
+            <Link to="/">
+             <p className='text-xl text-gray-400 hover:text-black'>Home <span>/</span></p>
+            </Link>
+            <p className='text-xl'>Account</p>
           </div>
-          <div>Welcome! <span className='text-[#DB4444]'>Md Rimel</span></div>
+          <div className='text-xl'>Welcome! <span className='text-[#DB4444] text-xl'>Md Rimel</span></div>
         </div>
-        <div>
-          <p className='font-semibold mt-10'>Manage My Account</p>
-          <div className='my-2.5 pl-8'>
-            <p className='text-[#DB4444]'>My Profile</p>
-            <p className='text-gray-400'>Address Book</p>
-            <p className='text-gray-400'>My Payment Option</p>
+        <div className='flex m-[70px]'>
+          <div>
+            <p className='font-semibold mt-10 text-xl py-3'>Manage My Account</p>
+            <div className='my-2.5 pl-8'>
+              <p className='text-[#DB4444] text-xl pb-2.5'>My Profile</p>
+              <p className='text-gray-400 text-xl pb-2.5'>Address Book</p>
+              <p className='text-gray-400 text-xl'>My Payment Option</p>
+            </div>
+            <p className='font-semibold text-xl py-3'>My Orders</p>
+            <div className='my-2.5 pl-8'>
+              <p className='text-gray-400 text-xl pb-2.5'>My Returns</p>
+              <p className='text-gray-400 text-xl pb-2.5'>My Cancellations</p>
+            </div>
+            <p className='font-semibold text-xl py-3'>My Wishlist</p>
           </div>
-          <p className='font-semibold'>My Orders</p>
-          <div className='my-2.5 pl-8'>
-            <p className='text-gray-400'>My Returns</p>
-            <p className='text-gray-400'>My Cancellations</p>
+
+
+          <div className='w-[870px] h-[630px]  m-20 py-8 px-14'>
+            <h2 className='text-xl text-[#DB4444] font-semibold pb-3'>Edit your Profile</h2>
+            <div className='flex w-[710px] justify-between'>
+              <div className='w-[330px]'>
+                <form action="">
+                  <p className='font-semibold'>First Name</p>
+                  <input type="text"
+                    placeholder='Md'
+                    className='focus:outline-none focus:border-black py-2 px-2' />
+                  <p className='mt-6 font-semi-bold'>Email</p>
+                  <input type="text"
+                    placeholder='rimel1111@gmail.com'
+                    className='focus:outline-none focus:border-black py-2 px-2' />
+                </form>
+              </div>
+              <div className='w-[330px]'>
+                <form action="">
+                  <p className='font-semibold'>Last Name</p>
+                  <input type="text"
+                    placeholder='Rimel'
+                    className='focus:outline-none focus:border-black py-2 px-2' />
+                  <p className='mt-6 font-semibold'>Address</p>
+                  <input type="text"
+                    placeholder='Kingston,5236,United State'
+                    className='focus:outline-none focus:border-black py-2 px-2' />
+                </form>
+              </div>
+            </div>
+            <div className='w-[710px] '>
+              <p className='font-semibold mt-7'>Password Changes</p>
+               <form action="">
+                  <input type="text"
+                    placeholder='Current Password'
+                    className='focus:outline-none focus:border-black py-5 px-2 w-full' />
+
+                  <input type="text"
+                    placeholder='New Password'
+                    className='focus:outline-none focus:border-black py-5 px-2 w-full' />
+
+                  <input type="text"
+                    placeholder='Confirm New Password'
+                    className='focus:outline-none focus:border-black py-5 px-2 w-full' />
+                </form>
+            </div>
+            <div className='flex justify-end right-10 items-center gap-3.5'>
+               <p className='text-xl'>Cancel</p>
+               <button className='bg-[#DB4444] w-[214px] h-[56px] rounded-sm'>
+                <p className='text-white text-xl'>Save Changes</p>
+               </button>
+            </div>
           </div>
-          <p className='font-semibold'>My Wishlist</p>
         </div>
       </div>
     </>
